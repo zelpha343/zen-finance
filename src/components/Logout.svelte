2a4a54ded49ef3d5a6c18c8343facb6b79d0c92b
zelpha-5W1H <tharunkothari@yahoo.com>
2023-06-28 22:00:23 +0530

    Second touches

---
+++ src/components/Logout.svelte
@@ -0,0 +1,3 @@
+<button class="fixed left-5 top-5 underline text-xl text-white hover:text-purple-400 p-3"
+	>Logout</button
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,26 @@
 	import NameDisplay from '../components/NameDisplay.svelte';
 	import Footer from '../components/Footer.svelte';
 	import Balance from '../components/Balance.svelte';
+	import Logout from '../components/Logout.svelte';
+
+	let width;
 </script>
 
-<div class="h-screen w-screen flex bg-stone-950 overflow-hidden">
-	<div class="flex flex-col w-full items-center">
-		<NameDisplay name="John Doe" />
-		<Logs />
+<svelte:window bind:innerWidth={width} />
+{#if width < 640}
+	<h1 class="text-8xl">Not supported on this device. Open this on a laptop or a desktop</h1>
+{:else}
+	<div class="h-screen w-screen flex bg-stone-950 overflow-hidden">
+		<div class="flex flex-col w-full items-center">
+			<NameDisplay name="John Doe" />
+			<Logs />
+			<button
+				class="bg-gradient-to-r from-fuchsia-600 to-pink-500 p-3 rounded-sm text-white hover:scale-105 shadow-2xl mt-6"
+				>Add Log?</button
+			>
+		</div>
+		<Footer />
+		<Balance />
+		<Logout />
 	</div>
-	<Footer />
-	<Balance />
-</div>
+{/if}
